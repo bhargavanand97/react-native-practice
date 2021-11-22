7904911fcd54ea3716eb6eb836cdd07ff19bd369
bhargavAnand <bhargav.a@emeasurematics.com>
2021-11-22 11:22:28 +0530

    Added changes to colors screen

--- src/screens/ColorsScreen.js
+++ src/screens/ColorsScreen.js
@@ -1,20 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
 import {View,Text,StyleSheet,Button, FlatList} from 'react-native';
 import ColorBox from '../Components/ColorBox';
 
+const reducer = (state,action) => {
+    const actionPayload = action.payload;
+    return [...state,  actionPayload]
+}
+
 const ColorsScreen = () => {
-    const [colors,setColors] = useState([]);
+    // const [colors,setColors] = useState([]);
+    const colors = []
+    const [state,dispatch] = useReducer(reducer,colors)
     
     return (
         <View>
             <Text>Colors Screen</Text>
             <Button title="Add a color" onPress = {()=> {
-                setColors([...colors, randomRgb()]);
+                // setColors([...colors, randomRgb()]);
+                dispatch({payload: randomRgb()})                
             }}/>
 
             <FlatList
                 keyExtractor = {(item) => item}
-                data = {colors} 
+                data = {state} 
                 renderItem = {({item}) => {
                     return <ColorBox randomColor = {item}/>
                 }}
